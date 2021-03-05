09312400a0d40b741559fd09b04a4c761ff23082
YuriiWest <jurii.west@gmail.com>
2021-03-05 16:01:24 +0200

    апдейт словаря

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import discord
 client = discord.Client()
-keywords = ["каеску"]
+keywords = ["каеску", "кс"]
 
 @client.event
 async def on_message(message):
     for i in range(len(keywords)):
         if keywords[i] in message.content:
             for j in range(1):
-                await message.channel.send("kek")
+                await message.channel.send("щас бы каеску бавити")
 
 client.run('ODE3MzkyNzA2Nzk5MTQwOTE1.YEI2Rw.CT3-r7vMg0rQw-FJ-_zC_os6xhs')
