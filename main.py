e0ec8b04b05f1b6bf2999cb7e97b0fda8536a466
YuriiWest <jurii.west@gmail.com>
2021-03-05 16:11:40 +0200

    апдейт словаря

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ async def on_message(message):
     for i in range(len(keywords)):
         if keywords[i] in message.content:
             for j in range(1):
-                await message.channel.send("щас бы каеску бавити")
+                await message.channel.send("щас бы коеску бавити")
 
 
 client.run('ODE3MzkyNzA2Nzk5MTQwOTE1.YEI2Rw.CT3-r7vMg0rQw-FJ-_zC_os6xhs')
